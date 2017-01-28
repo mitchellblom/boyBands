846d116dcbfbe33eac532882dfa45217087500db
Mitchell Blom <mitchellblom@gmail.com>
2017-01-28 09:38:11 -0600

    most of the way

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
-// The number of loops to perform (what if the array changes?)
+// The number of loops to perform (what if the array changes?) - aka find out the loop count
 var loopCount = 5;
 
-// Keep track of which band we're on in the loop
-var currentBand = "";
+if (bands.length > vegetables.length) {
+	loopcount = bands.length;
+} else {
+	loopcount = vegetables.length;
+}
 
-// Keep track of which veggie we're on in the loop
-var currentVeggie = "";
+// Keep track of which band we're on in the loop
+
+var currentBand = bands.loopcount;
+console.log("Current Band", currentBand);
+
+// // Keep track of which veggie we're on in the loop
+// var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 
   // Add the band names into the correct <div>
-  currentBand = ???;
-
-
-  // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  currentBand = bands[loopTracker];
+  bandElement.innerHTML += "<div>" + currentBand + "</div>";
+  console.log("Current Band", currentBand);
 
 }
+
+  // // Add the veggie names into the correct <div>
+  // currentVeggie = ???;
