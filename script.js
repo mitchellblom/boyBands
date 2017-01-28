9f987ea5f30954edf2182d486a07640bc46fd62e
Mitchell Blom <mitchellblom@gmail.com>
2017-01-28 13:50:26 -0600

    completed boyBands

--- script.js
+++ script.js
@@ -4,22 +4,20 @@
 // The number of loops to perform (what if the array changes?) - aka find out the loop count
 	var loopCount = 5;
 
-	if (bands.length > vegetables.length) {
-		loopcount = bands.length;
-	} else {
-		loopcount = vegetables.length;
-	}
-
 // Keep track of which band we're on in the loop
 
-	var currentBand = bands.loopcount;
-	console.log("Current Band", currentBand);
+	for (i = 0; i < bands.length; i++) {
+	var currentBand = bands[i];
+	console.log("Current Band:", currentBand);
+	}
 
 // Keep track of which veggie we're on in the loop
 // var currentVeggie = "";
 
-	var currentVegetable = vegetables.loopcount;
-	console.log("Current Vegetable", currentVegetable);
+	for (j = 0; j < vegetables.length; j++) {
+	var currentVeg = vegetables[j];
+	console.log("Current Vegetable:", currentVeg);
+	}
 
 // Get a reference to the appropriate DOM element for bands
 
@@ -30,19 +28,16 @@
 	var veggieElement = document.getElementById("vegetables");
 
 // Start looping
-	for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-
 // Add the band names into the correct <div>
+	for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 	  currentBand = bands[loopTracker];
 	  bandElement.innerHTML += "<div>" + currentBand + "</div>";
-	  console.log("Current Band", currentBand);
+	  console.log("Current Band:", currentBand);
 	}
 
 // currentVeggie = ???;
-
-	for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-
 // Add the vegetable names into the correct <div>
+	for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 	  currentVegetable = vegetables[loopTracker];
 	  veggieElement.innerHTML += "<div>" + currentVegetable + "</div>";
 	  console.log("Current Vegetable", currentVegetable);
