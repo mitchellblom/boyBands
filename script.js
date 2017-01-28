93fe10dc7e1373f48519a6adcc10f3186b9657f9
Mitchell Blom <mitchellblom@gmail.com>
2017-01-28 10:06:25 -0600

    printing but some undefines

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
-var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+	var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+	var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // The number of loops to perform (what if the array changes?) - aka find out the loop count
-var loopCount = 5;
+	var loopCount = 5;
 
-if (bands.length > vegetables.length) {
-	loopcount = bands.length;
-} else {
-	loopcount = vegetables.length;
-}
+	if (bands.length > vegetables.length) {
+		loopcount = bands.length;
+	} else {
+		loopcount = vegetables.length;
+	}
 
 // Keep track of which band we're on in the loop
 
-var currentBand = bands.loopcount;
-console.log("Current Band", currentBand);
+	var currentBand = bands.loopcount;
+	console.log("Current Band", currentBand);
 
-// // Keep track of which veggie we're on in the loop
+// Keep track of which veggie we're on in the loop
 // var currentVeggie = "";
 
+	var currentVegetable = vegetables.loopcount;
+	console.log("Current Vegetable", currentVegetable);
+
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById("boy-bands");
+
+	var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = document.getElementById("vegetables");
+
+	var veggieElement = document.getElementById("vegetables");
 
 // Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+	for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 
-  // Add the band names into the correct <div>
-  currentBand = bands[loopTracker];
-  bandElement.innerHTML += "<div>" + currentBand + "</div>";
-  console.log("Current Band", currentBand);
+// Add the band names into the correct <div>
+	  currentBand = bands[loopTracker];
+	  bandElement.innerHTML += "<div>" + currentBand + "</div>";
+	  console.log("Current Band", currentBand);
+	}
 
-}
+// currentVeggie = ???;
 
-  // // Add the veggie names into the correct <div>
-  // currentVeggie = ???;
+	for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+
+// Add the vegetable names into the correct <div>
+	  currentVegetable = vegetables[loopTracker];
+	  veggieElement.innerHTML += "<div>" + currentVegetable + "</div>";
+	  console.log("Current Vegetable", currentVegetable);
+	}
